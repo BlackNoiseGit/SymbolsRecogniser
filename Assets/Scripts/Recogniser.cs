3235c001a1fd3750934eb519c496b428014a55c1
BlackNoise <la_21@ukr.net>
2016-03-16 12:35:55 +0200

    another test

---
+++ Assets/Scripts/Recogniser.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using PDollarGestureRecognizer;
+public class Recogniser : MonoBehaviour {
+    
+    public delegate void RecogniseSymbolHandler(string str);
+
+    public event RecogniseSymbolHandler Recognised;
+
+    public bool isActive {get;set;}
+    
+    [SerializeField]
+    private Transform gestureOnScreenPrefab;
+
+    private List<Gesture> trainingSet = new List<Gesture>();
+
+    private List<Point> points = new List<Point>();
+    private int strokeId = -1;
+
+    private Vector3 virtualKeyPosition = Vector2.zero;
+    private Rect drawArea;
+
+    private RuntimePlatform platform;
+    private int vertexCount = 0;
+
+    private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
+    private LineRenderer currentGestureLineRenderer;
+
+    //GUI
+    private string message;
+    private bool recognized;
+ 
+
+    void Start()
+    {
+        isActive = false;
+        platform = Application.platform;
+        drawArea = new Rect(0, 0, Screen.width - Screen.width / 3, Screen.height);
+
+        string[] filePaths = Directory.GetFiles(Application.dataPath + "/Save", "*.xml");
+        foreach (string filePath in filePaths)
+            trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
+    }
+
+    void Update()
+    {
+        if (!isActive)
+            return;
+
+        if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+        {
+            if (Input.touchCount > 0)
+            {
+                virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            }
+        }
+
+        if (drawArea.Contains(virtualKeyPosition))
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (recognized)
+                {
+                    recognized = false;
+                    strokeId = -1;
+
+                    points.Clear();
+
+                    foreach (LineRenderer lineRenderer in gestureLinesRenderer)
+                    {
+
+                        lineRenderer.SetVertexCount(0);
+                        Destroy(lineRenderer.gameObject);
+                    }
+
+                    gestureLinesRenderer.Clear();
+                }
+
+                ++strokeId;
+
+                Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
+                currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
+
+                gestureLinesRenderer.Add(currentGestureLineRenderer);
+
+                vertexCount = 0;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
+
+                currentGestureLineRenderer.SetVertexCount(++vertexCount);
+                currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
+            }
+
+            if (Input.GetMouseButtonUp(0))
+                RecogniseSymbol();
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(drawArea, "Draw Area");
+    }
+
+    void RecogniseSymbol()
+    {
+        recognized = true;
+
+        Gesture candidate = new Gesture(points.ToArray());
+        Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
+
+        message = gestureResult.GestureClass;// + " " + gestureResult.Score;
+
+        if (Recognised != null)
+            Recognised(message);
+    }
+
+    public void ActivateRecogniser()
+    {
+        Invoke("DelayedActivationOfRecogniser", 0.2f);
+    }
+
+    private void DelayedActivationOfRecogniser()
+    {
+        isActive = true;
+    }
+}
